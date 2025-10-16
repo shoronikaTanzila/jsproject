97fdb2859b6585297051039cfdb6e1f138072371
SAADAT619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2025-10-17 05:14:38 +0600

    Initital commit

---
+++ operation.js
@@ -0,0 +1,23 @@
+let value=90
+let negative = -value
+console.log(negative);
+
+let str1 ="hello"
+let str2 ="tanzila"
+console.log(str1+str2);
+
+console.log("1"+2);
+
+console.log(2+"1"+2+4+5);
+console.log(2+3+"1"+2+4+5);
+console.log(+true);
+
+//console.log(true+);
+//console.log(true-);
+
+
+let game =100
+game++;
+console.log(game);
+
+
