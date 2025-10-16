28d31a1722b8fc1851b6ebd5bde2950584f599a8
SAADAT619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2025-10-17 05:40:59 +0600

    Initital commit

---
+++ 01_BASIC/compare.js
@@ -0,0 +1,46 @@
+console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+ // compare and equality check both are different in js 
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//strict check meaning === its not only check value but also check data type
+
+console.log("2"===2);
+
+
+//data type 
+
+
+
+//primitive
+
+
+// 7 type string ,number,boolean,null,undefined ,symbol (it's use for unique ness) ,bigint( sinetefic value
+
+
+
+
+// reference (non primitive))
+
+//arry,objects,function
+
+
+const isa =false 
+const out=null
+let useremail;
+const id1 =Symbol("123")
+const id2=Symbol("123")
+console.log(id);
+console.log(id2);
+
+
+
+
+
+
